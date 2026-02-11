8e23939c89f04de6dc4c93d4d8428de52a698141
tess3r4c7 <rishabhchowdhary2005@gmail.com>
2026-02-11 10:44:28 +0530

    project setup done

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
+server.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
